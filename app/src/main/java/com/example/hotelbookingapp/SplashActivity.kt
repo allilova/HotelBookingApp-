bf4f8b526a9877e71546492aec3edccfabeb85c0
allilova <lilova4p@gmail.com>
2026-03-24 20:42:10 +0200

    add logo

---
+++ app/src/main/java/com/example/hotelbookingapp/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.example.hotelbookingapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_DELAY_MS = 1800L
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        val fadeIn = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
+        fadeIn.duration = 800
+        findViewById<ImageView>(R.id.splashIcon).startAnimation(fadeIn)
+        findViewById<TextView>(R.id.splashAppName).startAnimation(fadeIn)
+        findViewById<TextView>(R.id.splashTagline).startAnimation(fadeIn)
+
+        lifecycleScope.launch {
+            delay(SPLASH_DELAY_MS)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
